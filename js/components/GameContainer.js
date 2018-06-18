d4ebee9fd5fd69be47a9c68600b9dfd75825066d
grady <jiangzhongjin@hotmail.com>
2018-06-18 11:20:47 +0800

    add PanResponder

--- js/components/GameContainer.js
+++ js/components/GameContainer.js
@@ -2,10 +2,12 @@ import React, { PureComponent } from 'react';
 import {
     View,
     StyleSheet,
-    Dimensions
+    Dimensions,
+    PanResponder
 } from 'react-native';
 import GridContainer from './GridContainer';
 import CardContainer from './CardContainer';
+import HadShowPositions from '../utils/HadShowPositions';
 
 const {width} = Dimensions.get('window');
 const styles = StyleSheet.create({
@@ -19,12 +21,94 @@ const styles = StyleSheet.create({
 
 export default class GameContainer extends PureComponent {
 
+    constructor(props) {
+        super(props);
+        this.panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: (e, gestureState) => this.handleStartShouldSetPanResponder(e, gestureState),
+            onMoveShouldSetPanResponder: (e, gestureState) => this.handleMoveShouldSetPanResponder(e, gestureState),
+            onPanResponderGrant: (e, gestureState) => this.handlePanResponderGrant(e, gestureState),
+            onPanResponderMove: (e, gestureState) => this.handlePanResponderMove(e, gestureState),
+            onPanResponderRelease: (e, gestureState) => this.handlePanResponderRelease(e, gestureState)
+        });
+
+        this.state = {
+            children: []
+        };
+        this.hadShowPositions = new HadShowPositions();
+    }
+
+
+    handleStartShouldSetPanResponder(e, gestureState) {
+        return true;
+    }
+
+    handleMoveShouldSetPanResponder(e, gestureState) {
+        return true;
+    }
+
+    handlePanResponderGrant(e, gestureState) {
+        console.log(`GameApp: handlePanResponderGrant`);
+    }
+
+    handlePanResponderMove(e, gestureState) {
+        console.log(`GameApp: handlePanResponderMove dx=${gestureState.dx}, dy= ${gestureState.dy}`);
+    }
+
+    handlePanResponderRelease(e, gestureState) {
+        console.log(`GameApp: handlePanResponderEnd dx=${gestureState.dx}, dy= ${gestureState.dy}`);
+    }
+
     render() {
         return (
-          <View style={styles.container}>
+          <View {...this.panResponder.panHandlers} style={styles.container}>
               <GridContainer />
-              <CardContainer children={this.props.children}/>
+              <CardContainer children={this.state.children}/>
           </View>
         );
+    }
+
+    componentWillMount() {
+        this.setUp();
+    }
+
+    setUp() {
+        this.setGameState()
+    }
+
+    setGameState() {
+        const children = this.getRandomCards();
+        this.setState({children : children})
+    }
+
+    getRandomCards() {
+        const ret = [];
+        for (let i = 0; i < this.props.startCard; i++) {
+            ret.push(this.getRandomCard())
+        }
+        return ret;
+    }
+
+    getRandomCard() {
+        let x, y;
+        do {
+            x = Math.floor(Math.random()*4);
+            y = Math.floor(Math.random()*4);
+            if (!this.hadShowPositions.isExist({x: x, y: y})) {
+                this.hadShowPositions.push({x: x, y: y});
+                break;
+            }
+        } while (true);
+
+        let value = Math.random() < 0.9 ? 2 : 4;
+        return {
+            value: value,
+            x: x,
+            y: y
+        }
+    }
+
+    restart() {
+        this.hadShowPositions.clear();
+        this.setGameState()
     }
 }
--- js/gameApp.js
+++ js/gameApp.js
@@ -7,7 +7,6 @@ import {
 import Header from './components/Header';
 import FunctionComponent from './components/FunctionComponent';
 import GameContainer from './components/GameContainer';
-import HadShowPositions from './utils/HadShowPositions';
 
 const {height, width} = Dimensions.get('window');
 
@@ -26,11 +25,7 @@ export default class GameApp extends PureComponent {
     constructor(props) {
         super(props);
         this.restart = this.restart.bind(this);
-        this.state = {
-            children: [{ x: 0, y: 0}, { x: 0, y: 3}]
-        };
         this.startCard = 2;
-        this.hadShowPositions = new HadShowPositions();
     }
 
     render() {
@@ -38,53 +33,12 @@ export default class GameApp extends PureComponent {
             <View style={styles.container}>
                 <Header/>
                 <FunctionComponent restart={this.restart}/>
-                <GameContainer children={this.state.children}/>
+                <GameContainer ref='gameContainer' startCard={this.startCard} />
             </View>
         )
     }
 
-    componentWillMount() {
-        this.setUp();
-    }
-
-    setUp() {
-        this.setGameState()
-    }
-
-    setGameState() {
-        const children = this.getRandomCards();
-        this.setState({children : children})
-    }
-
-    getRandomCards() {
-        const ret = [];
-        for (let i = 0; i < this.startCard; i++) {
-            ret.push(this.getRandomCard())
-        }
-        return ret;
-    }
-
-    getRandomCard() {
-        let x, y;
-        do {
-            x = Math.floor(Math.random()*4);
-            y = Math.floor(Math.random()*4);
-            if (!this.hadShowPositions.isExist({x: x, y: y})) {
-                this.hadShowPositions.push({x: x, y: y});
-                break;
-            }
-        } while (true);
-
-        let value = Math.random() < 0.9 ? 2 : 4;
-        return {
-            value: value,
-            x: x,
-            y: y
-        }
-    }
-
     restart() {
-        this.hadShowPositions.clear();
-        this.setGameState();
+        this.refs.gameContainer.restart();
     }
 }
